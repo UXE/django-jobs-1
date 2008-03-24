7ef2fd5b4153248a5d57e8576174b6155005c9f9
cliftoi <cliftoi@987ccdad-c2ba-db11-a1c9-001321b30054>
2008-03-24 19:27:36 +0000

    Basic desk attendant view/template done
    git-svn-id: file:///usr/local/svn/django/trunk@389 987ccdad-c2ba-db11-a1c9-001321b30054

--- desk_attendant/views.py
+++ desk_attendant/views.py
@@ -10,7 +10,17 @@ def index(request):
     """
     data = request.POST or None
     availability_form = AvailabilityForm(data)
+    reference_form1 = ReferenceForm(data)
+    reference_form2 = ReferenceForm(data)
+    reference_form3 = ReferenceForm(data)
+    placement_preference_form = PlacementPreferenceForm(data)
+    essay_response_form = EssayResponseForm(data)
 
-    return render_to_response('desk_attendant/applicant/index.html', 
-                              {'availability_form': availability_form,}, 
-                              context_instance=RequestContext(request))
+    return render_to_response('desk_attendant/applicant/index.html', {
+                              'availability_form': availability_form,
+                              'reference_form1': reference_form1,
+                              'reference_form2': reference_form2,
+                              'reference_form3': reference_form3,
+                              'placement_preference_form': placement_preference_form, 
+                              'essay_response_form': essay_response_form,
+                              }, context_instance=RequestContext(request))
