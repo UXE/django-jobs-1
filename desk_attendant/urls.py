51401a313c38fc93f38ced0260d24e7dbaa80851
huddlej <huddlej@987ccdad-c2ba-db11-a1c9-001321b30054>
2008-03-27 22:11:03 +0000

    fixed bad reference to job field name
    git-svn-id: file:///usr/local/svn/django/trunk@416 987ccdad-c2ba-db11-a1c9-001321b30054

--- desk_attendant/urls.py
+++ desk_attendant/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from wwu_housing.jobs.models import Job
 from views import index
 
-view_dict = {'job': Job.objects.filter(title='Desk Attendant').latest('open_date')}
+view_dict = {'job': Job.objects.filter(title='Desk Attendant').latest('open_datetime')}
 
 urlpatterns = patterns('',
     (r'^$', index, view_dict),
