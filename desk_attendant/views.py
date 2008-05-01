31a6d6be300060c8e4b35587c2e211829ac3e044
cliftoi <cliftoi@987ccdad-c2ba-db11-a1c9-001321b30054>
2008-05-01 22:52:38 +0000

    Added note for if applicant didn't submit resume, template now ignores statuses of 0
    git-svn-id: file:///usr/local/svn/django/trunk@666 987ccdad-c2ba-db11-a1c9-001321b30054

--- desk_attendant/views.py
+++ desk_attendant/views.py
@@ -276,7 +276,7 @@ def admin_individual(request, job, id):
     process_status, created = ApplicantStatus.objects.get_or_create(application=app, community=admin_community, name='process_status')
     process_status_form = ProcessStatusForm(data, instance=process_status, prefix='process_status')
 
-    if request.method == "POST":
+    if data:
         if process_status_form.is_valid():
             process_status_form.save()
         if hours_hired_for_form.is_valid():
