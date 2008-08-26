7e5fdf8c99060b02a9c733b6cba8a6a11783148a
huddlej <huddlej@987ccdad-c2ba-db11-a1c9-001321b30054>
2008-08-26 17:29:32 +0000

    commenting out bad code for now
    git-svn-id: file:///usr/local/svn/django/trunk@976 987ccdad-c2ba-db11-a1c9-001321b30054

--- desk_attendant/urls.py
+++ desk_attendant/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls.defaults import *
 from wwu_housing.jobs.models import Job
 from views import index, apply, application, admin_list, admin_individual, csv_export
 
-view_dict = {'job': Job.objects.filter(title='Desk Attendant').latest('open_datetime'),}
+# TODO: move this queryset into the index and store the job object in the user's session.
+#view_dict = {'job': Job.objects.filter(title='Desk Attendant').latest('open_datetime'),}
 
 urlpatterns = patterns('',
-    (r'^$', index, view_dict),
-    (r'^apply/$', apply, view_dict),
-    #(r'^application/(?P<id>\d+)/$', application),
-    (r'^admin/$', admin_list, view_dict),
-    (r'^admin/(?P<id>\d+)/$', admin_individual, view_dict)
-    #(r'^admin/export/$', csv_export, view_dict),
+#     (r'^$', index, view_dict),
+#     (r'^apply/$', apply, view_dict),
+#     #(r'^application/(?P<id>\d+)/$', application),
+#     (r'^admin/$', admin_list, view_dict),
+#     (r'^admin/(?P<id>\d+)/$', admin_individual, view_dict)
+#     #(r'^admin/export/$', csv_export, view_dict),
 )
