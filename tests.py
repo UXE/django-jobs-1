2a22e9636f189742344c8a035a880f16f478b339
huddlej <huddlej@987ccdad-c2ba-db11-a1c9-001321b30054>
2010-01-29 20:05:59 +0000

    added a more complex text for assign_reviewers which can't pass with the current design
    git-svn-id: file:///usr/local/svn/django/trunk@3508 987ccdad-c2ba-db11-a1c9-001321b30054

--- tests.py
+++ tests.py
@@ -56,3 +56,30 @@ class AssignReviewersTestCase(test.TestCase):
                                        rules=[lambda r, a, c: True])
         unassigned_applicants = assignments.get("_UNASSIGNED", [])
         self.assertEqual(len(unassigned_applicants), 0)
+
+    def test_two_reviewers_per_applicant(self):
+        """
+        Assigns two reviewers to each applicant.
+        """
+        applicants = self.applicants
+        applicants_per_reviewer = len(self.applicants) / len(self.reviewers) * 2
+
+        def two_reviewers_rule(reviewer, applicant, current_applicants):
+            if (len(current_applicants) < applicants_per_reviewer and applicant not in current_applicants):
+                return True
+            return False
+        
+        assignments = assign_reviewers(self.reviewers, self.applicants,
+                                       get_reviewer_key=self.get_reviewer_key,
+                                       rules=[two_reviewers_rule])
+        reviewers_by_applicant = {}
+        for reviewer, apps in assignments.items():
+            for app in apps:
+                if not app in reviewers_by_applicant:
+                    reviewers_by_applicant[app.name] = []
+                reviewers_by_applicant[app.name].append(reviewer)
+
+        self.assertTrue(all([len(reviewers) == 2
+                             for reviewers in reviewers_by_applicant.values()]),
+                        reviewers_by_applicant)
+
