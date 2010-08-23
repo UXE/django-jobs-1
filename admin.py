a52fcf2f7696d1c951d1a34fc55ec5bdd0007e24
John Huddleston <huddlej@wwu.edu>
2010-08-23 16:40:33 -0700

    updated jobs admin to reference newly renamed ApplicationComponentPart.

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from wwu_housing.jobs.models import Job, Applicant, Application, Date, \
-                                    Component, ApplicationComponent, \
+                                    Component, ApplicationComponentPart, \
                                     Qualification
 
 
@@ -10,7 +10,7 @@ class ComponentInline(admin.TabularInline):
 
 
 class JobAdmin(admin.ModelAdmin):
-    list_display = ("title", "open_datetime", "close_datetime", "description", 
+    list_display = ("title", "open_datetime", "close_datetime", "description",
                     "post_datetime")
     list_filter = ("open_datetime", "close_datetime")
     inlines = [ComponentInline]
@@ -24,14 +24,14 @@ class ApplicantAdmin(admin.ModelAdmin):
 admin.site.register(Applicant, ApplicantAdmin)
 
 
-class ApplicationComponentInline(admin.TabularInline):
-    model = ApplicationComponent
+class ApplicationComponentPartInline(admin.TabularInline):
+    model = ApplicationComponentPart
 
 
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ("applicant", "job", "start_datetime", "end_datetime")
     list_filter = ("job", "start_datetime", "end_datetime")
-    inlines = [ApplicationComponentInline]
+    inlines = [ApplicationComponentPartInline]
 admin.site.register(Application, ApplicationAdmin)
 
 
