6bb9aa0a4cf5f374727cdf816d8099f1176e5b0c
John Huddleston <huddlej@wwu.edu>
2010-08-25 16:43:29 -0700

    added code to make unpublished_job test pass.

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from models import Job
 
 
 def job(request, job_name):
-    return HttpResponse(job_name)
+    job = get_object_or_404(Job.objects.posted(), slug=job_name)
+    return HttpResponse(job.title)
