a989933bd4f745268fefaaac5f28603d65c452ac
John Huddleston <huddlej@wwu.edu>
2010-08-25 15:30:15 -0700

    added a url pattern for generically handling a job site.

--- urls.py
+++ urls.py
@@ -2,8 +2,10 @@ from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list
 
 from models import Job
+from views import job
 
 
 urlpatterns = patterns("",
     url(r"^$", object_list, {"queryset": Job.objects.posted(), "template_object_name": "job"}, name="jobs_index"),
+    url(r"^(?P<job_name>[-\w]+)/$", job, name="jobs_job")
 )
