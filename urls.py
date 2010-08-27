a4d0e1cc0f18d5991cd96fc8a56a37adf2ccde4e
John Huddleston <huddlej@wwu.edu>
2010-08-27 13:53:32 -0700

    added code to make application url test pass.

--- urls.py
+++ urls.py
@@ -2,10 +2,11 @@ from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list
 
 from models import Job
-from views import job
+from views import job, application
 
 
 urlpatterns = patterns("",
     url(r"^$", object_list, {"queryset": Job.objects.posted(), "template_object_name": "job"}, name="jobs_index"),
-    url(r"^(?P<job_slug>[-\w]+)/$", job, name="jobs_job")
+    url(r"^(?P<job_slug>[-\w]+)/$", job, name="jobs_job"),
+    url(r"^(?P<job_slug>[-\w]+)/application/$", application, name="jobs_application")
 )
--- views.py
+++ views.py
@@ -7,3 +7,8 @@ from models import Job
 def job(request, job_slug):
     job = get_object_or_404(Job.objects.posted(), slug=job_slug)
     return HttpResponse(job.title)
+
+
+def application(request, job_slug):
+    job = get_object_or_404(Job.objects.posted(), slug=job_slug)
+    return HttpResponse(job.title)
