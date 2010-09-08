50606ecee309f47afd53d7240e232afece786340
John Huddleston <huddlej@wwu.edu>
2010-09-08 15:14:55 -0700

    added code to make tests pass in ComponentTestCase

--- views.py
+++ views.py
@@ -61,4 +61,8 @@ def component(request, job_slug, component_slug):
 
     component = get_object_or_404(job.component_set, slug=component_slug)
 
-    return HttpResponse(component.name)
+    return render_to_response(
+        component.get_template(),
+        {"component": component},
+        context_instance=RequestContext(request)
+    )
