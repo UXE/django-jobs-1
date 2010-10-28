c52b651320d97821b76b0697e2916a9bc1285fca
Firass Asad <Firass.Asad@wwu.edu>
2010-10-28 11:33:55 -0700

    wrap logic in a try block in case the application component part doesn't exist

--- templatetags/jobs.py
+++ templatetags/jobs.py
@@ -1,5 +1,7 @@
 from django import template
 
+from wwu_housing.jobs.models import ApplicationComponentPart
+
 register = template.Library()
 
 @register.simple_tag
@@ -12,24 +14,25 @@ def status(application, component):
     is_complete = []
     activity_date = None
     for component_part in component.componentpart_set.all():
-        application_component_part = application.applicationcomponentpart_set.get(
-            component_part=component_part
-        )
+        try:
+            application_component_part = application.applicationcomponentpart_set.get(
+                component_part=component_part
+            )
 
-        if application_component_part.content_object:
-            is_complete.append(True)
+            if application_component_part.content_object:
+                is_complete.append(True)
 
-            if not activity_date:
-                activity_date = application_component_part.activity_date
-            elif application_component_part.activity_date > activity_date:
-                activity_date = application_component_part.activity_date
-
-        else:
+                if not activity_date:
+                    activity_date = application_component_part.activity_date
+                elif application_component_part.activity_date > activity_date:
+                    activity_date = application_component_part.activity_date
+            else:
+                is_complete.append(False)
+        except ApplicationComponentPart.DoesNotExist:
             is_complete.append(False)
 
     if all(is_complete):
         return "<strong>Completed</strong> on %s" % activity_date.strftime("%A, %B %e at %I:%M %p")
-        #return "Completed: %s" % activity_date.strftime("%m/%d/%Y %I:%M")
     elif True in is_complete:
         return "<strong>Started</strong>, last modified on %s" % activity_date.strftime("%A, %B %e at %I:%M %p")
     else:
