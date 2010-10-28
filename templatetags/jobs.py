b1f3675b46aed56f46d8fe564020128f72076ab5
Firass Asad <Firass.Asad@wwu.edu>
2010-10-28 09:20:13 -0700

    buddy developed status template tag with dcole and lohrb

--- templatetags/jobs.py
+++ templatetags/jobs.py
@@ -9,4 +9,29 @@ def status(application, component):
 
     Returns the status of the particular component.
     """
-    return u"&mdash;"
+    is_complete = []
+    activity_date = None
+    for component_part in component.componentpart_set.all():
+        application_component_part = application.applicationcomponentpart_set.get(
+            component_part=component_part
+        )
+
+        if application_component_part.content_object:
+            is_complete.append(True)
+
+            if not activity_date:
+                activity_date = application_component_part.activity_date
+            elif application_component_part.activity_date > activity_date:
+                activity_date = application_component_part.activity_date
+
+        else:
+            is_complete.append(False)
+
+    if all(is_complete):
+        return "<strong>Completed</strong> on %s" % activity_date.strftime("%A, %B %e at %I:%M %p")
+        #return "Completed: %s" % activity_date.strftime("%m/%d/%Y %I:%M")
+    elif True in is_complete:
+        return "<strong>Started</strong>, last modified on %s" % activity_date.strftime("%A, %B %e at %I:%M %p")
+    else:
+        return u"&mdash;"
+
