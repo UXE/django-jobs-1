8ec3d7ca1ccc4e787a33da9147b48f005f75bbba
Firass Asad <Firass.Asad@wwu.edu>
2010-12-16 12:07:13 -0800

    don't want to override eq

--- models.py
+++ models.py
@@ -274,12 +274,6 @@ class ApplicationStatus(models.Model):
     def __unicode__(self):
         return self.status
 
-    def __eq__(self, other):
-        if isinstance(other, basestring):
-            return self.status == other
-        else:
-            return self.status == other.status
-
 
 ###TODO### Rename this AdminStatus
 class AdminApplication(models.Model):
