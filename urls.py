fccd4021916349a65b53af69720b9971fd47189d
reak2 <kamira.rea@wwu.edu>
2011-04-18 11:23:43 -0700

    Reroute desk-attendant admin.

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list
 
-from wwu_housing.wwu_jobs.views import interview, positionplacement  #interview_creation
-from wwu_housing.desk_attendant.views  import apply
+from wwu_housing.wwu_jobs.views import interview, positionplacement, interview_creation
+from wwu_housing.desk_attendant.views  import apply, admin_list
 from models import Job
 from views import admin, applicant, application, component, create_admin_csv, export_application, job, jobs_index
 
@@ -12,8 +12,9 @@ urlpatterns = patterns("",
     url(r"^$", jobs_index, name="jobs_index"),
     url(r"^(?P<job_slug>[-\w]+)/$", job, name="jobs_job"),
     url(r"^(?P<job_slug>[-\w]+)/interview/$", interview, name="jobs_interview"),
-#    url(r"^(?P<job_slug>[-\w]+)/interview_creation/$", interview_creation, name="jobs_interview_creation"),
+    url(r"^(?P<job_slug>[-\w]+)/interview_creation/$", interview_creation, name="jobs_interview_creation"),
     url(r"^(?P<job_slug>[-\w]+)/admin/positionplacement/$", positionplacement, name="jobs_positionplacement"),
+    url(r"^desk-attendant/admin/$", admin_list, name="da_admin_list"),
     url(r"^(?P<job_slug>[-\w]+)/admin/$", admin, name="jobs_job_admin"),
     url(r"^(?P<job_slug>[-\w]+)/admin/csv/$", create_admin_csv, name="jobs_job_admin_csv"),
     url(r"^(?P<job_slug>[-\w]+)/admin/export_application/$", export_application, name="jobs_job__export_application"),
